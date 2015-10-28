77679f8e570cdf5d43f390ef154de1388e8969e1
Erin Meyers <erinmeyers88@gmail.com>
2015-10-28 08:56:01 -0600

    Prettied up.

---
+++ js/mainCtrl.js
@@ -0,0 +1,26 @@
+angular.module("quoteBook")
+	.controller("mainCtrl", function ($scope, dataService) {
+		
+		$scope.showNewInput = false;
+		
+		$scope.getQuotes = function () {
+			$scope.quotes = dataService.getData();
+		}();
+		
+		
+		$scope.add = function () {
+			$scope.showNewInput = !$scope.showNewInput;
+			if ($scope.newQuote.text) {
+				$scope.quotes = dataService.addData($scope.newQuote);
+				$scope.newQuote = {};
+				//$scope.showNewInput = false;
+			}
+		}
+		
+		
+		$scope.remove = function () {
+			$scope.removeQuote = dataService.removeData();
+		}
+		
+		
+	});
